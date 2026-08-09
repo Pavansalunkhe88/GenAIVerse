fcce42dc8b8e98540edfc9880a703316beea6b42
Pavan Salunkhe <pavansalunkhe17@gmail.com>
2026-08-09 16:23:06 +0530

    Add initial implementation of main.py and requirements files for API integration

---
+++ API_Integration/main.py
@@ -0,0 +1,18 @@
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+client = OpenAI()
+
+response = client.chat.completions.create(
+    model="gpt-4o",
+    messages=[
+        {
+            "role": "user",
+            "content": "Hey there"
+        }
+    ]
+)
+
+print(response.choices[0].message.content)
