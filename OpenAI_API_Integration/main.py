f88a17dbfa75f7245cc471d1f07b73760fa89cec
Pavan Salunkhe <pavansalunkhe17@gmail.com>
2026-08-10 11:11:35 +0530

    Update API integration to include GEMINI_API_KEY and adjust model in main.py

--- OpenAI_API_Integration/main.py
+++ OpenAI_API_Integration/main.py
@@ -1,12 +1,16 @@
 from dotenv import load_dotenv
 from openai import OpenAI
+import os
 
 load_dotenv()
 
-client = OpenAI()
+client = OpenAI(
+    api_key=os.getenv("GEMINI_API_KEY"),
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
 
 response = client.chat.completions.create(
-    model="gpt-4o",
+    model="gemini-2.5-flash",
     messages=[
         {
             "role": "user",
